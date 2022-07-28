2088f98a9f36b0394e4ecb508451110fff38fd23
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2022-07-29 00:11:21 +0100

    the great shrinkening

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,16 @@
-{ isContainer }:
 { pkgs, config, lib, modulesPath, ... }:
+let
+  systemdMini = pkgs.systemdMinimal.override {
+    withAnalyze = true;
+    withApparmor = true;
+    withLogind = true;
+    withTimesyncd = true;
+    withCoredump = true;
+    withCompression = true;
+    withFido2 = false;
+    withTpm2Tss = false;
+  };
+in
 {
   imports = [
     "${modulesPath}/profiles/minimal.nix"
@@ -14,10 +25,14 @@
   '';
 
   systemd = {
+    package = systemdMini;
     services = {
       nix-daemon.enable = false;
       mount-pstore.enable = false;
       sysctl.enable = false;
+      journald.enable = false;
+      user-sessions.enable = false;
+      shutdownRamfs.enable = false;
     };
     sockets.nix-daemon.enable = false;
   };
@@ -25,51 +40,142 @@
   services = {
     openssh = {
       enable = true;
+      startWhenNeeded = true;
+      permitRootLogin = "yes";
     };
-    timesyncd.enable = false;
+    # timesyncd.enable = false;
     udisks2.enable = false;
-    getty.autologinUser = "root";
+    # getty.autologinUser = "root";
+    nscd.config = lib.replaceStrings
+      [ "server-user             nscd" ]
+      [ "server-user             root" ]
+      (builtins.readFile "${modulesPath}/services/system/nscd.conf");
+    # config.services.nscd.config;
+    # ''
+    #   server-user             root
+
+    #   enable-cache            passwd          yes
+    #   positive-time-to-live   passwd          0
+    #   negative-time-to-live   passwd          0
+    #   shared                  passwd          yes
+
+    #   enable-cache            group           yes
+    #   positive-time-to-live   group           0
+    #   negative-time-to-live   group           0
+    #   shared                  group           yes
+
+    #   enable-cache            netgroup        yes
+    #   positive-time-to-live   netgroup        0
+    #   negative-time-to-live   netgroup        0
+    #   shared                  netgroup        yes
+
+    #   enable-cache            hosts           yes
+    #   positive-time-to-live   hosts           0
+    #   negative-time-to-live   hosts           0
+    #   shared                  hosts           yes
+
+    #   enable-cache            services        yes
+    #   positive-time-to-live   services        0
+    #   negative-time-to-live   services        0
+    #   shared                  services        yes
+    # '';
   };
 
   users.users.root = {
     password = "root";
   };
 
-  system.activationScripts = {
-    installInitScript = ''
-      mkdir -p /sbin
-      ln -fs $systemConfig/init /sbin/init
-    '';
+  system = {
+    stateVersion = "22.05";
+    activationScripts = {
+      installInitScript = ''
+        mkdir -p /sbin
+        ln -fs $systemConfig/init /sbin/init
+      '';
+    };
   };
 
-  environment.systemPackages = with pkgs; [
-    nano
-    bash
-    vim
-    wget
-    curl
+  environment = {
+    defaultPackages = with pkgs; [
+      nano
+      curl
+      nerdctl
+      iptables
+      coreutils
+      time
+      which
+      gnused
+      gnutar
+      gzip
+      less
+      getent
+      gawk
+      findutils
+      netcat
+    ];
+  };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      iptables = super.iptables-legacy;
+      containerd = (pkgs.callPackage "${pkgs.path}/pkgs/applications/virtualization/containerd" {
+        btrfs-progs = null;
+      });
+      openssh = (super.openssh.overrideAttrs (final: prev: {
+        doCheck = false;
+      })).override {
+        withFIDO = false;
+        withKerberos = false;
+      };
+      nerdctl = super.nerdctl.overrideAttrs (final: prev: {
+        postInstall = ''
+          wrapProgram $out/bin/nerdctl \
+            --prefix CNI_PATH : "${pkgs.cni-plugins}/bin"
+          installShellCompletion --cmd nerdctl \
+            --bash <($out/bin/nerdctl completion bash) \
+            --fish <($out/bin/nerdctl completion fish) \
+            --zsh <($out/bin/nerdctl completion zsh)
+        '';
+      });
+      systemdStage1 = systemdMini;
+      # systemdMinimal = systemdMini;
+      # ffs this is causing a dup
+    })
   ];
 
   networking = {
     hostName = "nixos";
+    resolvconf.enable = false;
     dhcpcd.enable = false;
     firewall.enable = false;
     wireless.enable = false;
   };
 
-  virtualisation.docker.enable = true;
+  virtualisation = {
+    docker = {
+      enable = false;
+      enableOnBoot = false;
+    };
+    podman = {
+      # enable = true;
+      # dockerCompat = true;
+    };
+    containerd = {
+      enable = true;
+    };
+  };
 
   security = {
+    audit.enable = false;
     polkit.enable = false;
+    sudo.enable = false;
   };
 
   boot = {
     kernelParams = [ "console=ttyS0" "noapic" "reboot=k" "panic=1" "pci=off" "nomodules" "rw" "init=/nix/var/nix/profiles/system/init" ];
-    isContainer = isContainer;
     loader.grub.enable = false;
     initrd.includeDefaultModules = false;
     initrd.availableKernelModules = lib.mkForce [ ];
-    # initrd.availableKernelModules = [ "md_mod" "raid0" "raid1" "raid10" "raid456" "ahci" ];
     kernelModules = [ "dm-mod" ];
     kernelPackages =
       let
@@ -80,7 +186,6 @@
         inherit (pkgs) stdenv;
         inherit (pkgs) lib;
         inherit version;
-        # inherit (base) src;
         src = pkgs.fetchurl {
           url = "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${version}.tar.xz";
           sha256 = "sha256-QLdNCULyVdoHSBcQ4Qg0EtBuN+Rbj52eNK6FbbN7lSc=";
@@ -90,26 +195,25 @@
       });
   };
 
-  fileSystems."/" = {
-    device = "/dev/vda";
-    # options = [ "ro" ];
+  fileSystems = {
+    "/" = {
+      device = "/dev/vda";
+      # options = [ "ro" ];
+      neededForBoot = true;
+    };
   };
 
-  boot.postBootCommands =
-    ''
-      # After booting, register the contents of the Nix store in the Nix
-      # database.
-      if [ -f /nix-path-registration ]; then
-        ${config.nix.package.out}/bin/nix-store --load-db \
-          < /nix-path-registration &&
-        rm /nix-path-registration
-      fi
-      # nixos-rebuild also requires a "system" profile
-      ${config.nix.package.out}/bin/nix-env -p /nix/var/nix/profiles/system \
-        --set /run/current-system
-    '';
+  boot.postBootCommands = ''
+    # After booting, register the contents of the Nix store in the Nix database.
+    if [ -f /nix-path-registration ]; then
+      ${config.nix.package.out}/bin/nix-store --load-db \
+        < /nix-path-registration &&
+      rm /nix-path-registration
+    fi
+    # nixos-rebuild also requires a "system" profile
+    ${config.nix.package.out}/bin/nix-env -p /nix/var/nix/profiles/system \
+      --set /run/current-system
+  '';
 
   nix.gc.automatic = true;
-
-  system.stateVersion = "22.05";
 }
--- flake.nix
+++ flake.nix
@@ -1,52 +1,39 @@
 {
   inputs = {
     nixpkgs.url = github:nixos/nixpkgs/nixos-22.05;
-    nixos-generators = {
-      url = "github:nix-community/nixos-generators";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, nixos-generators, ... }:
-    let
-      firecrackerSystem = { isContainer }:
-        nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            (import ./configuration.nix {
-              isContainer = isContainer;
-            })
-          ];
-        };
-    in
+  outputs = { self, nixpkgs, ... }:
     {
       nixosConfigurations = {
-        firecracker-container =
-          firecrackerSystem { isContainer = true; };
-
-        firecracker =
-          firecrackerSystem { isContainer = false; };
+        firecracker = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./configuration.nix ];
+        };
       };
 
       packages.x86_64-linux = {
         firecracker-vmlinux =
-          let system = self.nixosConfigurations.firecracker;
-          in system.config.system.build.kernel.dev;
+          let
+            system = self.nixosConfigurations.firecracker;
+          in
+          system.config.system.build.kernel.dev;
 
         firecracker-rootfs =
           let
-            system = self.nixosConfigurations.firecracker-container;
+            system = self.nixosConfigurations.firecracker;
           in
           import "${nixpkgs}/nixos/lib/make-disk-image.nix" {
             pkgs = nixpkgs.legacyPackages.x86_64-linux;
-            lib = nixpkgs.lib;
+            inherit (nixpkgs) lib;
             diskSize = "auto";
             config = system.config;
-            additionalSpace = "2G";
+            additionalSpace = "1K";
             format = "raw";
             partitionTableType = "none";
             installBootLoader = false;
             fsType = "ext4";
+            copyChannel = false;
           };
       };
     };
